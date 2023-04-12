33eaf58407a1889ecd9799cfcbda8ca12070b9a0
NicBeans <102036624+NicBeans@users.noreply.github.com>
2023-04-12 16:06:38 +0200

    Update mpv_yt.sh
    created loop

--- mpv_yt.sh
+++ mpv_yt.sh
@@ -6,16 +6,23 @@ three_days_ago=$(date -d "3 days ago" "+%Y-%m-%d")
 # Update ytcc subscriptions
 ytcc update
 
-# List videos from the last three days, sorted by date (newest to oldest)
-videos=$(ytcc list --since "$three_days_ago" --order-by publish_date desc)
+# Loop until user decides to exit
+while true; do
+    # List videos from the last three days, sorted by date (newest to oldest)
+    videos=$(ytcc list --since "$three_days_ago" --order-by publish_date desc)
 
-# Display the videos and prompt the user to select one
-echo "List of videos from the last three days:"
-echo "$videos"
-echo
-read -p "Enter the number of the video you want to play: " video_number
+    # Display the videos and prompt the user to select one
+    echo "List of videos from the last three days:"
+    echo "$videos"
+    echo
+    read -p "Enter the number of the video you want to play, or type 'exit' to quit: " video_number
 
-# Get the video URL and play it using mpv (or another video player of your choice)
-# video_url=$(ytcc list --since "$three_days_ago" --order-by publish_date desc --attributes url | sed -n "${video_number}p" | tr -d '[:blank:]')
-# mpv "$video_url"
-ytcc play $video_number
+    # Check if the user wants to exit the script
+    if [[ "$video_number" == "exit" ]]; then
+        break
+    fi
+
+    # Get the video URL and play it using mpv
+    ytcc play $video_number
+done
+
